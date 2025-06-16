37afbb189467b4bcaf363152708c7479a4948d0e
Marcelo B. de Arruda <marruda@cjf.jus.br>
2025-06-16 18:19:02 -0300

    fix: standardize port configuration for staging and production environments

--- rhbk-spa/src/environments/environment.ts
+++ rhbk-spa/src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
   keycloak: {
-    url: 'http://localhost:8080',
-    //url: 'https://sso-dev.apps.opshifthomol.cjf.jus.br',
-    realm: 'demo',
+    //url: 'http://localhost:8080',
+    url: 'https://sso-dev.apps.opshifthomol.cjf.jus.br',
+    realm: 'JF',
     clientId: 'rhbk-spa-client',
   },
 };
